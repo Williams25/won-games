3fcc84bc75b94e00d2c3b64df12e0139984aab84
Williams25 <william.souza.timao@hotmail.com>
2022-02-22 09:05:23 -0300

    passando theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { GlobalStyle } from "styles/global";
+import { ThemeProvider } from "styled-components";
+import theme from "styles/theme";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={{ ...theme }}>
       <Head>
         <title>React avançado</title>
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
