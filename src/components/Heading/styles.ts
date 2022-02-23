e78b313f0ff850ebcb43961d76d9c27a92db053e
Williams25 <william.souza.timao@hotmail.com>
2022-02-23 10:48:53 -0300

    estilizando component

---
+++ src/components/Heading/styles.ts
@@ -0,0 +1,37 @@
+import styled, { css, DefaultTheme } from "styled-components";
+import media from "styled-media-query";
+import { HeadingProps } from ".";
+
+const wrapperModifiers = {
+  lineLeft: (theme: DefaultTheme) => css`
+    padding-left: ${theme.spacings.xxsmall};
+    border-left: 0.7rem solid ${theme.colors.secondary};
+  `,
+  lineBottom: (theme: DefaultTheme) => css`
+    position: relative;
+    margin-bottom: ${theme.spacings.medium};
+
+    &::before {
+      position: absolute;
+      bottom: -0.5rem;
+      left: 0;
+      content: "";
+      width: 5rem;
+      border: 0.4rem solid ${theme.colors.secondary};
+    }
+  `
+};
+
+export const Heading = styled.h2<HeadingProps>`
+  ${({ theme, color, lineLeft, lineBottom }) => css`
+    font-size: ${theme.font.sizes.xlarge};
+    color: ${theme.colors[color!]};
+
+    ${media.greaterThan("medium")`
+    font-size: ${theme.font.sizes.xxlarge};
+    `};
+
+    ${lineLeft && wrapperModifiers.lineLeft(theme)}
+    ${lineBottom && wrapperModifiers.lineBottom(theme)}
+  `}
+`;
