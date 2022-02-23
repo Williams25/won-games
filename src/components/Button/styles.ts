772faf7284d483b6f25baee727df0edf6121a9b7
Williams25 <william.souza.timao@hotmail.com>
2022-02-23 13:20:14 -0300

    estilizando buttom

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,2 +1,40 @@
-import styled from "styled-components";
-export const Button = styled.main``;
+import styled, { css, DefaultTheme } from "styled-components";
+import { ButtonProps } from ".";
+
+type WrapperProps = Pick<ButtonProps, "size">;
+
+const buttonModifiers = {
+  small: (theme: DefaultTheme) => css`
+    height: 3rem;
+    font-size: ${theme.font.sizes.xsmall};
+  `,
+  large: (theme: DefaultTheme) => css`
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+    height: 4rem;
+    font-size: ${theme.font.sizes.small};
+  `,
+  medium: (theme: DefaultTheme) => css`
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
+    height: 5rem;
+    font-size: ${theme.font.sizes.medium};
+  `
+};
+
+export const Button = styled.button<WrapperProps>`
+  ${({ theme, size }) => css`
+    background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
+    color: ${theme.colors.white};
+    border: 0;
+    border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xxsmall};
+    cursor: pointer;
+    transition: filter 0.5s;
+    transition-delay: 0.1s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+
+    ${!!size && buttonModifiers[size](theme)}
+  `}
+`;
