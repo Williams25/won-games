6a0d767c099e038b83cc0015d8e07b1eb9e8ff23
Williams25 <william.souza.timao@hotmail.com>
2022-02-28 12:59:36 -0300

    ajustando component e criando testes para cubrir as moificações

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,21 +1,33 @@
 import { ReactNode } from "react";
 import * as S from "./styles";
 
+export type LineColors = "primary" | "secondary";
+
 export type HeadingProps = {
   children: ReactNode;
   color?: "white" | "black";
+  size?: "small" | "medium";
   lineLeft?: boolean;
   lineBottom?: boolean;
+  lineColor?: LineColors;
 };
 
 export const Heading = ({
   children,
   color = "white",
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = "medium",
+  lineColor = "primary"
 }: HeadingProps) => {
   return (
-    <S.Heading color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+    <S.Heading
+      color={color}
+      lineLeft={lineLeft}
+      lineBottom={lineBottom}
+      size={size}
+      lineColor={lineColor}
+    >
       {children}
     </S.Heading>
   );
