fb9b7a3d5af1c9422d22a0e2f99d15c9f6a1b495
Williams25 <william.souza.timao@hotmail.com>
2022-02-28 19:45:54 -0300

    realizando cobrimento de testes

---
+++ src/components/Ribbon/Ribbon.test.tsx
@@ -0,0 +1,45 @@
+import { screen } from "@testing-library/react";
+import { Ribbon } from "./index";
+import { renderWithTheme } from "utils/tests/helpers";
+
+describe("Ribbon", () => {
+  it("should render the text correctly", async () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>);
+
+    expect(screen.getByText(/Best Seller/gi)).toBeInTheDocument();
+  });
+
+  it("should render with primary color", async () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>);
+
+    expect(screen.getByText(/Best Seller/gi)).toHaveStyle({
+      "background-color": "#f231a5"
+    });
+  });
+
+  it("should render with secondary color", async () => {
+    renderWithTheme(<Ribbon color="secondary">Best Seller</Ribbon>);
+
+    expect(screen.getByText(/Best Seller/gi)).toHaveStyle({
+      "background-color": "#3cd3c1"
+    });
+  });
+
+  it("should render with the normal size as dafault", async () => {
+    renderWithTheme(<Ribbon>Best Seller</Ribbon>);
+
+    expect(screen.getByText(/Best seller/gi)).toHaveStyle({
+      height: "3.6rem",
+      "font-size": "1.4rem"
+    });
+  });
+
+  it("should render with the small size", async () => {
+    renderWithTheme(<Ribbon size="small">Best Seller</Ribbon>);
+
+    expect(screen.getByText(/Best seller/gi)).toHaveStyle({
+      height: "2.6rem",
+      "font-size": "1.2rem"
+    });
+  });
+});
