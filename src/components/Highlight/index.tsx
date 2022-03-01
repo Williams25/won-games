16823fffaf3a0c7fcb2f942b74b3f3851c528bf2
Williams25 <william.souza.timao@hotmail.com>
2022-03-01 14:12:50 -0300

    ajustando estrutura

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -6,21 +6,25 @@ export type HighlightProps = {
   subTitle: string;
   buttonLabel: string;
   buttonLink: string;
+  backgroundImage: string;
 };
 
 export const Highlight = ({
   subTitle,
   title,
   buttonLabel,
-  buttonLink
+  buttonLink,
+  backgroundImage
 }: HighlightProps) => {
   return (
-    <S.Highlight>
-      <S.Title>{title}</S.Title>
-      <S.SubTitle>{subTitle}</S.SubTitle>
-      <Button as="a" href={buttonLink}>
-        {buttonLabel}
-      </Button>
+    <S.Highlight backgroundImage={backgroundImage}>
+      <S.Content>
+        <S.Title>{title}</S.Title>
+        <S.SubTitle>{subTitle}</S.SubTitle>
+        <Button as="a" href={buttonLink}>
+          {buttonLabel}
+        </Button>
+      </S.Content>
     </S.Highlight>
   );
 };
