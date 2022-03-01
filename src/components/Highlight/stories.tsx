20d852a69e004ac5a395edead2ddd82cf295759c
Williams25 <william.souza.timao@hotmail.com>
2022-03-01 14:35:37 -0300

    adicionando floatimage

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -16,4 +16,13 @@ export default {
   }
 } as Meta;
 
-export const Basic: Story<HighlightProps> = (args) => <Highlight {...args} />;
+export const Basic: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: "104rem", margin: "0 auto" }}>
+    <Highlight {...args} />
+  </div>
+);
+export const WithFloatimage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: "104rem", margin: "0 auto" }}>
+    <Highlight {...args} floatImage="/img/red-dead-float.png" />
+  </div>
+);
