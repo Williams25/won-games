d1fdfbeca10006b23a3820709ba871324a22113a
Williams25 <william.souza.timao@hotmail.com>
2022-03-01 14:13:10 -0300

    passando imagem de background para os stories

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -4,11 +4,15 @@ import { Highlight, HighlightProps } from "./index";
 export default {
   title: "Highlight",
   component: Highlight,
+  parameters: {
+    layout: "fullscreen"
+  },
   args: {
     title: "Heading 1",
     subTitle: "Heading 2",
     buttonLabel: "Buy now",
-    buttonLink: "/"
+    buttonLink: "/",
+    backgroundImage: "/img/red-dead-img.jpg"
   }
 } as Meta;
 
