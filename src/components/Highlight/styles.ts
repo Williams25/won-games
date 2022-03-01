f2654b662b7718d164d94cf6da508a84a824d58a
Williams25 <william.souza.timao@hotmail.com>
2022-03-01 14:59:48 -0300

    finalizando estyles

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -2,18 +2,43 @@ import styled, { css } from "styled-components";
 import media from "styled-media-query";
 import { HighlightProps } from ".";
 
-type WrapperProps = Pick<HighlightProps, "backgroundImage">;
+type WrapperProps = Pick<HighlightProps, "backgroundImage" | "alignment">;
 
+const wrapperModifiers = {
+  right: () => css`
+    grid-template-areas: "floatimage content";
+    grid-template-columns: 1.3fr 2fr;
+
+    ${Content} {
+      text-align: right;
+    }
+
+    ${FloatImage} {
+      justify-self: start;
+    }
+  `,
+  left: () => css`
+    grid-template-areas: "content floatimage";
+    grid-template-columns: 2fr 1.3fr;
+
+    ${Content} {
+      text-align: left;
+    }
+
+    ${FloatImage} {
+      justify-self: end;
+    }
+  `
+};
 export const Highlight = styled.section<WrapperProps>`
-  ${({ backgroundImage }) => css`
+  ${({ backgroundImage, alignment }) => css`
     position: relative;
     background-image: url(${backgroundImage});
     background-position: center center;
     background-size: cover;
     height: 23rem;
     display: grid;
-    grid-template-areas: "floatimage content";
-    grid-template-columns: 1.3fr 2fr;
+    ${!!alignment && wrapperModifiers[alignment]}
 
     &::after {
       content: "";
@@ -33,7 +58,6 @@ export const Content = styled.div`
   ${({ theme }) => css`
     grid-area: content;
     z-index: ${theme.layers.base};
-    text-align: right;
     padding: ${theme.spacings.xsmall};
 
     ${media.greaterThan("medium")`
