45d59e7d06b52ed6236300f504ae82a90347f26d
Williams25 <william.souza.timao@hotmail.com>
2022-03-02 18:17:58 -0300

    criando testes para verificar se o slider esta carregando os items

--- src/components/Slider/Slider.test.tsx
+++ src/components/Slider/Slider.test.tsx
@@ -1,15 +1,26 @@
+import "../../../.jest/match-media-mock";
 import { screen } from "@testing-library/react";
 import { renderWithTheme } from "utils/tests/helpers";
 
 import { Slider } from "./index";
 
 describe("Slider", () => {
-it("should render the heading", async () => {
-renderWithTheme(
-<Slider />
-);
-expect(
-screen.getByRole("heading", { name: /Slider/i })
-).toBeInTheDocument();
-});
+  it("should render children as slider item", async () => {
+    const { container } = renderWithTheme(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>item 1</p>
+        <p>item 2</p>
+      </Slider>
+    );
+
+    expect(
+      screen.getByText(/item 1/gi).parentElement?.parentElement
+    ).toHaveClass("slick-slide");
+
+    expect(
+      screen.getByText(/item 2/gi).parentElement?.parentElement
+    ).toHaveClass("slick-slide");
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
