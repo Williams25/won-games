6cc5e232e9b3a4bd15327224f74eafe561b444a8
Williams25 <william.souza.timao@hotmail.com>
2022-03-02 17:58:00 -0300

    criando estrutura para os stories

--- src/components/Slider/stories.tsx
+++ src/components/Slider/stories.tsx
@@ -1,9 +1,57 @@
 import { Meta, Story } from "@storybook/react/types-6-0";
-import { Slider } from "./index";
+import styled from "styled-components";
+import { Slider, SliderProps, SliderSettings } from "./index";
 
 export default {
   title: "Slider",
-  component: Slider,
+  component: Slider
 } as Meta;
 
-export const Basic: Story = (args) => <Slider {...args} />;
+const settings: SliderSettings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1
+};
+
+const Slide = styled.div`
+  background: gray;
+  width: 30rem;
+  padding: 10rem 0;
+  border: 0.1rem solid red;
+  color: white;
+  text-align: center;
+`;
+
+export const Horizontal: Story<SliderProps> = () => (
+  <Slider settings={settings}>
+    <Slide>1</Slide>
+    <Slide>2</Slide>
+    <Slide>3</Slide>
+    <Slide>4</Slide>
+    <Slide>5</Slide>
+    <Slide>6</Slide>
+  </Slider>
+);
+
+const verticalSettings: SliderSettings = {
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  vertical: true,
+  verticalSwiping: true,
+  infinite: false,
+  dots: true
+};
+
+export const Vertical: Story<SliderProps> = () => (
+  <Slider settings={verticalSettings}>
+    <Slide>1</Slide>
+    <Slide>2</Slide>
+    <Slide>3</Slide>
+    <Slide>4</Slide>
+    <Slide>5</Slide>
+    <Slide>6</Slide>
+  </Slider>
+);
