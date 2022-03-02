eb94915833b7d33a00dfec0b5be4ccf530979132
Williams25 <william.souza.timao@hotmail.com>
2022-03-01 21:16:02 -0300

    realizando testes para o favorito

--- src/components/GameCard/GameCard.test.tsx
+++ src/components/GameCard/GameCard.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/react";
+import { screen, fireEvent } from "@testing-library/react";
 import { renderWithTheme } from "utils/tests/helpers";
 import theme from "styles/theme";
 import { GameCard } from "./index";
@@ -41,19 +41,34 @@ describe("GameCard", () => {
     expect(price).not.toHaveStyle({ textDecoration: "line-through" });
     expect(price).toHaveStyle({ backgroundColor: theme.colors.secondary });
   });
-});
 
-it("should render a line-through in price when promotional", () => {
-  // renderiza o componente (COM promotionalPrice) // 235 reais // 15 reais
-  renderWithTheme(<GameCard {...props} promotionalPrice="R$ 15,00" />);
+  it("should render a line-through in price when promotional", () => {
+    // renderiza o componente (COM promotionalPrice) // 235 reais // 15 reais
+    renderWithTheme(<GameCard {...props} promotionalPrice="R$ 15,00" />);
 
-  // preço tenha line-through (235)
-  expect(screen.getByText("R$ 235,00")).toHaveStyle({
-    textDecoration: "line-through"
+    // preço tenha line-through (235)
+    expect(screen.getByText("R$ 235,00")).toHaveStyle({
+      textDecoration: "line-through"
+    });
+
+    // preço novo promocional não vai ter line-through (15)
+    expect(screen.getByText("R$ 15,00")).not.toHaveStyle({
+      textDecoration: "line-through"
+    });
   });
 
-  // preço novo promocional não vai ter line-through (15)
-  expect(screen.getByText("R$ 15,00")).not.toHaveStyle({
-    textDecoration: "line-through"
+  it("should render a filled favorite icon when favorite is true", () => {
+    renderWithTheme(<GameCard {...props} favorite={true} />);
+
+    expect(screen.getByLabelText(/remove to wish list/gi)).toBeInTheDocument();
+  });
+
+  it("should call onFav method when favorite is clicked", () => {
+    const onFav = jest.fn();
+    renderWithTheme(<GameCard {...props} favorite={true} onFav={onFav} />);
+
+    fireEvent.click(screen.getAllByRole("button")[0]);
+
+    expect(onFav).toBeCalled();
   });
 });
