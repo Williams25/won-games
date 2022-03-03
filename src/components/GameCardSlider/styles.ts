6c806ea33ac84803b237d8b73347efb830e69ec2
Williams25 <william.souza.timao@hotmail.com>
2022-03-03 19:39:47 -0300

    estilizando componente

---
+++ src/components/GameCardSlider/styles.ts
@@ -0,0 +1,85 @@
+import styled, { css } from "styled-components";
+import media from "styled-media-query";
+import { GameCardSliderProps } from ".";
+
+type WrapperProps = Pick<GameCardSliderProps, "color">;
+
+export const GameCardSlider = styled.section<WrapperProps>`
+  ${({ theme, color }) => css`
+    ${media.lessThan("huge")`
+    overflow-x: hidden;
+  `}
+    .slick-track,
+    .slick-list {
+      display: flex;
+    }
+    .slick-slide > div {
+      margin: 0 ${theme.spacings.xxsmall};
+      flex: 1 0 auto;
+      height: 100%;
+    }
+    .slick-list {
+      margin: 0 -${theme.spacings.xxsmall};
+    }
+    ${media.greaterThan("large")`
+    .slick-slide > div {
+      margin: 0 ${theme.spacings.xsmall};
+    }
+    .slick-list {
+      margin: 0 -${theme.spacings.xsmall};
+    }
+  `}
+    .slick-prev,
+    .slick-next {
+      display: block;
+      color: ${theme.colors[color!]};
+      cursor: pointer;
+      position: absolute;
+      top: 50%;
+      width: 2.5rem;
+      height: 2.5rem;
+      padding: 0;
+      transform: translate(0, -50%);
+    }
+    .slick-prev {
+      left: -${theme.spacings.xxlarge};
+    }
+    .slick-next {
+      right: -${theme.spacings.xxlarge};
+    }
+    .slick-prev.slick-disabled,
+    .slick-next.slick-disabled {
+      visibility: hidden;
+    }
+
+    .slick-dots {
+      list-style: none;
+      display: flex !important;
+      align-items: center;
+      justify-content: center;
+      margin-top: ${theme.spacings.small};
+      overflow: hidden;
+
+      li {
+        background: ${theme.colors.white};
+        width: 1.2rem;
+        height: 1.2rem;
+        border-radius: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: 0 ${theme.spacings.xxsmall};
+        cursor: pointer;
+        &.slick-active {
+          background: ${theme.colors.primary};
+        }
+      }
+      button {
+        opacity: 0;
+        width: 1.2rem;
+        height: 1.2rem;
+        cursor: pointer;
+      }
+    }
+  `}
+`;
