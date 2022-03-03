008ad9587db8253973c52babd0709f15166b7d6a
Williams25 <william.souza.timao@hotmail.com>
2022-03-03 18:54:52 -0300

    ajustando stories

--- src/components/BannerSlider/stories.tsx
+++ src/components/BannerSlider/stories.tsx
@@ -41,7 +41,7 @@ export default {
 } as Meta;
 
 export const Basic: Story<BannerSliderProps> = (args) => (
-  <div style={{ maxWidth: "104rem", margin: "0 auto" }}>
+  <div style={{ maxWidth: "130rem", margin: "0 auto" }}>
     <BannerSlider {...args} />
   </div>
 );
